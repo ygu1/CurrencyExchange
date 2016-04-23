2a867d1711915639dc3a787ebc141b1d41340cc6
EERC718 -ADM <EERC718-adm@rover-221-182.rovernet.mtu.edu>
2016-04-23 19:56:46 -0400

    button and textlabel

--- CurrencyExchange/ViewController.swift
+++ CurrencyExchange/ViewController.swift
@@ -27,6 +27,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var currencyDict: NSMutableDictionary! = NSMutableDictionary()
     var currencyDictKeys: Array<String>! = Array<String>()
     
+    var currency1: Double!
+    var currency2: Double!
+    var btnSelected: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -39,15 +43,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         getAllCurrency()
     }
     
+    
+    /*
+     *   Show or hide table
+     */
     @IBAction func showHiddenTable(sender: UIButton) {
         if nameTable.hidden {
             nameTable.hidden = false
+            let tempFrame = CGRect(x: tableBtn1.frame.origin.x, y: tableBtn1.frame.origin.y, width: nameTable.frame.size.width, height: nameTable.frame.size.height)
+            nameTable.frame = tempFrame
         }
         else {
-            nameTable.hidden = true
+            //nameTable.hidden = true
         }
     }
     
+    /*
+     *   Set button title
+     */
+    func setBtn(moneyName:String, btnNumber:Int){
+        if btnNumber == 1 {
+            tableBtn1.setTitle(moneyName, forState: .Normal)
+        }
+        else {
+            tableBtn2.setTitle(moneyName, forState: .Normal)
+        }
+    }
+    
+    func setTextLabel(){
+        let key1 = tableBtn1.currentTitle!
+        let key2 = tableBtn2.currentTitle!
+        currency1 = currencyDict.valueForKey(key1) as! Double
+        currency2 = currencyDict.valueForKey(key2) as! Double
+        inputText1.text = "1.00"
+        inputText2.text = String(format:"%.2f",1.0 * currency2 / currency1)
+        
+    }
+    
+    /*
+     *   Set table appearance
+     */
     func setTable() {
         self.nameTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         nameTable.delegate = self
@@ -58,13 +93,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         nameTable.layer.cornerRadius = 5
     }
     
+    /*
+     *   Number of sections in the table
+     */
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
+    
+    /*
+     *   Number of rows in the table
+     */
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return currencyDict.count
     }
     
+    /*
+     *   Set cell in the table
+     */
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell : UITableViewCell = nameTable.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
         let key = currencyDictKeys[indexPath.row] as String
@@ -76,11 +121,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    /*
+     *   Get currency info by http request, updating the database
+     */
     func getAllCurrency() {
-//        spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
-//        spinner.center = self.view.center
-//        self.view.addSubview(spinner)
-//        spinner.startAnimating()
+        spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        spinner.center = self.view.center
+        self.view.addSubview(spinner)
+        spinner.startAnimating()
         let requestURL: NSURL = NSURL(string: urlPath)!
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
         urlRequest.HTTPMethod = "GET"
@@ -122,6 +170,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         task.resume()
     }
     
+    /*
+     *   Upate the temp currency dictinary
+     */
     func updateCurrencyDict() {
         let tempCurrency = Currency.fetchAll(self.managedObjectContext)
         if tempCurrency != nil && tempCurrency?.count != 0{
@@ -130,16 +181,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 currencyDict.setObject(tempCurrency![i].currency, forKey: tempCurrency![i].name!)
                 i += 1
             }
+            currencyDict.setObject(1.0, forKey: "EUR")
             currencyDictKeys = (currencyDict.allKeys as! Array<String>).sort()
         }
         else {
             print("Please connect network to update currency database.")
         }
-        //self.spinner.stopAnimating()
-        print(currencyDictKeys)
+        //print(currencyDictKeys)
         dispatch_async(dispatch_get_main_queue()){
+            self.spinner.stopAnimating()
             self.nameTable.reloadData()
         }
+        self.setBtn("EUR", btnNumber: 1)
+        self.setBtn("USD", btnNumber: 2)
+        self.setTextLabel()
     }
     
     override func didReceiveMemoryWarning() {
