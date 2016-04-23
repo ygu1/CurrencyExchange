f831edf056649d2c7c8a9d34a3811fcc394a26c4
EERC718 -ADM <EERC718-adm@rover-221-182.rovernet.mtu.edu>
2016-04-22 21:57:32 -0400

    add http request function to get currency

--- CurrencyExchange/ViewController.swift
+++ CurrencyExchange/ViewController.swift
@@ -12,6 +12,10 @@ import CoreData
 class ViewController: UIViewController {
     
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    let urlPath: String = "http://api.fixer.io/latest"
+    var spinner: UIActivityIndicatorView!
+    var allCurrency: NSMutableArray = NSMutableArray()
+    var data: NSMutableData!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +25,49 @@ class ViewController: UIViewController {
 //        let paths = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
 //        let documentsURL = paths[0] as! NSURL
 //        print(documentsURL)
-
-        Currency.modifyCurrency(self.managedObjectContext, moneyName: "USD", nowCurrency: 1.2)
+        //Currency.modifyCurrency(self.managedObjectContext, moneyName: "USD", nowCurrency: 1.2)
+        getAllCurrency()
     
+    }
+    
+    func getAllCurrency() {
+        spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        spinner.center = self.view.center
+        self.view.addSubview(spinner)
+        spinner.startAnimating()
+        let url = NSURL(string:urlPath)!
+        var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "GET"
+        data = NSMutableData()
+        var connection = NSURLConnection(request: request, delegate: self, startImmediately: true)
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse)
+    { //It says the response started coming
+        NSLog("didReceiveResponse")
+    }
+    func connection(connection: NSURLConnection, didReceiveData _data: NSData)
+    { //This will be called again and again until you get the full response
+        //NSLog("didReceiveData")
+        // Appending data
+        data.appendData(_data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection)
+    {
+        // This will be called when the data loading is finished i.e. there is no data left to be received and now you can process the data.
+        spinner.stopAnimating()
+        NSLog("connectionDidFinishLoading")
+        var jsonError: NSError?
+        let hcards = (try! NSJSONSerialization.JSONObjectWithData(data, options: [])) as! NSMutableDictionary
+        print("\(hcards)")
+        //cards = hcards.valueForKey("Goblins vs Gnomes") as! NSMutableArray
+        //println("\(cards)")
+        dispatch_async(dispatch_get_main_queue()){
+            //self.tableView.reloadData()
+        }
+        
+        //var responseStr:NSString = NSString(data:self.data, encoding:NSUTF8StringEncoding)
     }
     
     override func didReceiveMemoryWarning() {
